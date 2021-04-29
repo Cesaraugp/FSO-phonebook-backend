4a9c8686c513bc988a271990fc552dbbd276b966
Cesar Pérez <cesaraugp01@gmail.com>
2021-04-28 20:30:15 -0400

    3.2 Phonebook backend step2 ✔

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  const date = new Date();
+  const amount = persons.length;
+  res.send(`<p>Phonebook has info for ${amount} people</p>
+            <p>${date}</p>
+    `);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
