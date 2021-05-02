6393d5b4092e1767506e6064b1ea6fc5ea6244d6
Cesar Pérez <cesaraugp01@gmail.com>
2021-05-02 11:56:51 -0400

    3.10 phonebook backend step10 ✔

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const unknownEndpoint = (request, res) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
